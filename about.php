d95f2abb6fa990d64626c03460071f07a77ed0a2
Akagra-Mish <71135222+Akagra-Mish@users.noreply.github.com>
2020-10-21 19:56:22 +0530

    my official website

---
+++ about.php
@@ -0,0 +1,84 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>About-Us</title>
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css">
+    <link href="https://fonts.googleapis.com/css2?family=Josefin+Sans:ital,wght@0,400;1,300&display=swap" rel="stylesheet">   
+    <link rel="stylesheet" type="text/css" href="style.css">
+    <link rel="shortcut icon" type="image/jpg" href="images/logo.png" />
+</head>
+<body>
+
+<nav class="navbar navbar-expand-lg navbar-dark bg-dark">
+  <a class="navbar-brand" href="#">Techz GameCoding</a>
+  <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+    <span class="navbar-toggler-icon"></span>
+  </button>
+
+  <div class="collapse navbar-collapse" id="navbarSupportedContent">
+    <ul class="navbar-nav ml-auto">
+      <li class="nav-item active">
+        <a class="nav-link" href="index.php">Home <span class="sr-only">(current)</span></a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link" href="about.php">About-Us</a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link" href="contact.php">Contact-Us</a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link" href="services.html">Services</a>
+      </li>
+      
+    </ul>
+    <form class="form-inline my-2 my-lg-0">
+      <input class="form-control mr-sm-2" type="search" placeholder="Search" aria-label="Search">
+      <button class="btn btn-outline-success my-2 my-sm-0" type="submit">Search</button>
+    </form>
+  </div>
+</nav>
+
+<div class="jumbotron jumbotron-fluid">
+  <div class="container">
+    <h1>Techz GameCoding</h1>
+    <p>we the programmer, Please Like,Share and Subscribe</p>
+  </div>
+</div>
+
+<section class="my-5">
+    <div class="py-5">
+        <h2 class="text-center"> About Us </h2>
+    </div>
+   <div class="container-fluid">
+    <div class="row">
+        <div class="col-lg-6 col-md-6 col-12">
+            <img src="images/forest.jpg" class="img-fluid aboutimg" >
+         </div>
+         <div class="col-lg-6 col-md-6 col-12">
+            <h2>I am Techz GameCoding</h2>
+            <p>We Techz GameCoding is a youtube channel of 
+            Akagra Mishra,India.He learned coding from WhitehatJr.com.He is well specialised in Backend of 
+            any Website or in Mobile Application and Teaches coding to Student in a different and Interesting 
+            way. Akagra web development cources are very cheap and very effective way.I created my youtube 
+            channel on 26/7/2018, But after sometime I stopped uploading videos on it. If you like my Youtube 
+            Channel please subscribe.Hey guys this is Techz GameCoding please subscribe, I upload videos on 
+            coding and gaming,This is my attempt to tech basics and those coding techniques to people in short
+             time which took me a long time to learn. Techz GameCoding, I provide a quick and to the point demo 
+             along with resources of anything and everything I teach. I provide source code(if any) in the 
+             description of every video if requested. Quality programming videos in English :)). Channel link<a href="https://www.youtube.com/c/TechzGameCoding?sub_confirmation=1">
+            Techz GameCoding</a></p>
+
+               <a href="about.php" class="btn btn-success"> Check More </a>      
+         </div>
+    </div>
+</div>
+</section>
+
+<footer>
+   <p  class="p-4 bg-dark text-white text-center">@techzgamecoding</p>
+</footer>
+
+</body>
+</html>
---
+++ index.php
@@ -0,0 +1,186 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Home</title>
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css">
+    <link href="https://fonts.googleapis.com/css2?family=Josefin+Sans:ital,wght@0,400;1,300&display=swap" rel="stylesheet">    <link rel="stylesheet" type="text/css" href="st.css">
+    <link rel="shortcut icon" type="image/jpg" href="images/logo.png" />
+</head>
+<body>
+
+<nav class="navbar navbar-expand-lg navbar-dark bg-dark">
+  <a class="navbar-brand" href="#">Techz GameCoding</a>
+  <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+    <span class="navbar-toggler-icon"></span>
+  </button>
+
+  <div class="collapse navbar-collapse" id="navbarSupportedContent">
+    <ul class="navbar-nav ml-auto">
+      <li class="nav-item active">
+        <a class="nav-link" href="index.php">Home <span class="sr-only">(current)</span></a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link" href="about.php">About-Us</a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link" href="contact.php">Contact-Us</a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link" href="services.html">Services</a>
+      </li>
+      
+    </ul>
+    <form class="form-inline my-2 my-lg-0">
+      <input class="form-control mr-sm-2" type="search" placeholder="Search" aria-label="Search">
+      <button class="btn btn-outline-success my-2 my-sm-0" type="submit">Search</button>
+    </form>
+  </div>
+</nav>
+
+<div id="demo" class="carousel slide" data-ride="carousel">
+  <ul class="carousel-indicators">
+    <li data-target="#demo" data-slide-to="0" class="active"></li>
+    <li data-target="#demo" data-slide-to="1"></li>
+    <li data-target="#demo" data-slide-to="2"></li>
+  </ul>
+  <div class="carousel-inner">
+    <div class="carousel-item active">
+      <img src="images/los.jpg" alt="Los Angeles" width="1100" height="500">
+      <div class="carousel-caption">
+        <h3>Los Angeles</h3>
+        <p>We had such a great time in LA!</p>
+      </div>   
+    </div>
+    <div class="carousel-item">
+      <img src="images/chi.jpeg" alt="Chicago" width="1100" height="500">
+      <div class="carousel-caption">
+        <h3>Chicago</h3>
+        <p>Thank you, Chicago!</p>
+      </div>   
+    </div>
+    <div class="carousel-item">
+      <img src="images/new.jpg" alt="New York" width="1100" height="500">
+      <div class="carousel-caption">
+        <h3>New York</h3>
+        <p>We love the Big Apple!</p>
+      </div>   
+    </div>
+  </div>
+  <a class="carousel-control-prev" href="#demo" data-slide="prev">
+    <span class="carousel-control-prev-icon"></span>
+  </a>
+  <a class="carousel-control-next" href="#demo" data-slide="next">
+    <span class="carousel-control-next-icon"></span>
+  </a>
+</div>
+
+<section class="my-5">
+    <div class="py-5">
+        <h2 class="text-center"> About Us </h2>
+    </div>
+   <div class="container-fluid">
+    <div class="row">
+        <div class="col-lg-6 col-md-6 col-12">
+            <img src="images/forest.jpg" class="img-fluid aboutimg" >
+         </div>
+         <div class="col-lg-6 col-md-6 col-12">
+            <h2>I am Techz GameCoding</h2>
+            <p>We Techz GameCoding is a youtube channel of Akagra Mishra,India.He 
+            learned coding from WhitehatJr.com.He is well specialised in Backend of any Website or in Mobile 
+            Application and Teaches coding to Student in a different and Interesting way. Akagra web development 
+            cources are very cheap and very effective way.I created my youtube channel on 26/7/2018, But after sometime 
+          I stopped uploading videos on it. If you like my <b>Youtube</b> Channel please subscribe.Hey guys this is Techz GameCoding please subscribe, I upload videos on coding and gaming,This is my attempt to tech basics and those coding techniques to people in short time which took me a long time to learn.
+Techz GameCoding, I provide a quick and to the point demo along with resources of anything and everything I teach. I provide source code(if any) in the description of every video if requested.
+Quality programming videos in English :)).
+        Channel link <a href="https://www.youtube.com/c/TechzGameCoding?sub_confirmation=1" target="_blank">Techz GameCoding</a></p>
+
+               <a href="about.php" class="btn btn-success"> Check More</a>      
+         </div>
+    </div>
+</div>
+</section>
+
+<section class="my-5">
+    <div class="py-5">
+        <h2 class="text-center"> Language Gallery </h2>
+</div>
+
+<div class="container-fluid">
+    <div class="row">
+        <div class="col-lg-4 col-md-4 col-12">
+            <img src="images/erlang.jpg" class="img-fluid pb-4">
+</div>
+        <div class="col-lg-4 col-md-4 col-12">
+            <img src="images/mysql.jpg" class="img-fluid pb-4">
+</div>
+        <div class="col-lg-4 col-md-4 col-12">
+            <img src="images/ruby.jpg" class="img-fluid pb-4">
+</div>
+        <div class="col-lg-4 col-md-4 col-12">
+            <img src="images/js.jpg" class="img-fluid pb-4">
+</div>
+        <div class="col-lg-4 col-md-4 col-12">
+            <img src="images/java.jpg" class="img-fluid pb-4">
+</div>
+        <div class="col-lg-4 col-md-4 col-12">
+            <img src="images/html.jpg" class="img-fluid pb-4">
+</div>
+        <div class="col-lg-4 col-md-4 col-12">
+            <img src="images/css.jpg" class="img-fluid pb-4">
+</div>
+        <div class="col-lg-4 col-md-4 col-12">
+            <img src="images/python.JPG" class="img-fluid pb-4">
+</div>
+        <div class="col-lg-4 col-md-4 col-12">
+            <img src="images/php.jpg" class="img-fluid pb-4">
+        </div>
+    </div>
+</div>
+
+</section> 
+
+<section class="my-5">
+    <div class="py-5">
+        <h2 class="text-center"> Contact-Us </h2>
+</div>
+<div class="w-50 m-auto">
+   <form action="userinfo.php" method="post">
+      <div class="form-group">
+        <label>Username</label>
+        <input type="text" name="user" class="form-control" autocomplete="off">
+     </div>
+     <div class="form-group">
+        <label>E-mail</label>
+        <input type="text" name="email" class="form-control" autocomplete="off">
+     </div>
+     <div class="form-group">
+        <label>Mobile</label>
+        <input type="text" name="mobile" class="form-control" autocomplete="off">
+     </div>
+     <div class="form-group">
+        <label>Comments</label>
+        <textarea class="form-control" name="comment">
+        </textarea>
+     </div>
+     <button type="submit" class="btn btn-primary">Submit</button>
+   </form>
+</div>
+</section>
+
+<footer>
+   <p  class="p-4 bg-dark text-white text-center">@techzgamecoding</p>
+</footer>
+
+
+
+
+
+
+
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
+  <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.16.0/umd/popper.min.js"></script>
+  <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js"></script>    
+</body>
+</html>
